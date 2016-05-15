f7de88594b1cbf682547181f11aa1b31a9874421
Quy LD <quy-ld@trente.asia>
2016-05-15 15:36:50 +0700

    quy commit

---
+++ CustomXIBSwift/CSPickerView..swift
@@ -0,0 +1,71 @@
+//
+//  TestView.swift
+//  pickerkViewSwift
+//
+//  Created by quy on 5/15/16.
+//  Copyright © 2016 quy. All rights reserved.
+//
+
+import UIKit
+protocol CSPickerViewDelegate {
+    func donePicker(month:Int, year: Int)
+}
+
+@IBDesignable class CSPickerView: UIView {
+     var delegate : CSPickerViewDelegate?
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib()
+    }
+    
+    @IBOutlet weak var pickerView: MonthYearPickerView!
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib()
+    }
+   
+   
+    @IBOutlet weak var titleEx: UILabel!
+
+    @IBAction func hidePickerView(sender: AnyObject) {
+       self.hidden = true
+       
+    }
+    @IBAction func doneButtonClicked(sender: AnyObject) {
+        self.delegate?.donePicker(pickerView.month, year: pickerView.year)
+        self.hidden = true
+    }
+    @IBOutlet weak var donePickerView: UIButton!
+    @IBInspectable var lblTitleText : String?
+        {
+        get{
+            return titleEx.text;
+        }
+        set(lblTitleText)
+        {
+            titleEx.text = lblTitleText!;
+        }
+    }
+
+    
+       func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "CSPickerView", bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        view.frame = bounds
+        view.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(view);
+        
+        
+        
+    }
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- CustomXIBSwift/ViewController.swift
+++ CustomXIBSwift/ViewController.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CSPickerViewDelegate {
 
-    @IBOutlet weak var customView: SimpleCustomView!
+ 
+    @IBAction func showPickerView(sender: AnyObject){
+        self.customView.hidden = false
+    }
+    @IBOutlet weak var showPicker: UIButton!
+    @IBOutlet weak var customView: CSPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-   self.customView.lblTitle.text = "I am a custom view using xib!";
-
+        customView.delegate = self
+        customView.titleEx.text = "aaaaaa"
+//    self.view.backgroundColor = (UIColor(red: 255/0, green: 255/0, blue: 255/0, alpha: 0.5))
+//
+//    
     }
-
+    func donePicker(month: Int, year: Int) {
+        print(month)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
